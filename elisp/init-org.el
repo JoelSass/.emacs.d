9626bc543869bd40877646506edf563a53e2a46d
Joel Sass <joelsass@pm.me>
2021-09-12 17:17:57 +0200

    Fix: fixed encoding issues and now pin packages

--- elisp/init-org.el
+++ elisp/init-org.el
@@ -50,6 +50,13 @@
 '(org-level-5 ((t (:height 1.05 ))))
 )
 
+
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-language-environment 'utf-8)
+(set-selection-coding-system 'utf-8)
+(set-language-environment "UTF-8")
+
 (use-package org-roam
       :ensure t
       :custom
